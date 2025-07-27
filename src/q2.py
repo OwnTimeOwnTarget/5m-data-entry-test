668016287c1345576c70424fb9ae4b37185ead3e
OwnTimeOwnTarget <ongkahkeat81@gmail.com>
2025-07-27 11:12:45 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -5,10 +5,16 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
+    for index,value in enumerate(lst):
+        if value = find_val:
+            lst[index] = replace_val
+    
+    return lst
 
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+find_and_replace(["apple", "banana", "apple"], "apple", "orange")
