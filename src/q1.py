dd5061cdbcc7a12da26ee46bda5d6b6f114026ec
OwnTimeOwnTarget <ongkahkeat81@gmail.com>
2025-07-27 11:00:20 +0800

    Update q1.py

--- src/q1.py
+++ src/q1.py
@@ -6,6 +6,10 @@ def swap(x, y):
     - Return -1 if x and y is not numeric, and
     - print the swapped values if both x and y are numeric.
     """
+    if isinstance(x, int) and isinstance(y, int):
+        print(y,x)
+    else:
+        return -1
     return
 
 
@@ -13,3 +17,5 @@ def swap(x, y):
 # Invoke the function "swap" using the following scenarios:
 # - "Apple", 10
 # - 9, 17
+swap("Apple", 10)
+swap(9, 17)
